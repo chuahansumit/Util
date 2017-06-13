e5a2441f0999f6992b4d951e2db356febc858e6b
Chohan <devsyc@ttx.com>
2017-06-13 13:57:51 -0500

    ee

--- util.js
+++ util.js
@@ -6,3 +6,7 @@ function JsonItem(jsonString,key)
 { 
   return JSON.stringify(JSON.parse(jsonString)[key]);
 }
+function GetRecentBuildId(jsonString,key)
+{ 
+  return JSON.parse(jsonString).value[0].id;
+}
