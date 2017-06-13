a0f40c09f8c742385eadf59bda5cd594989fa2b9
Chohan <devsyc@ttx.com>
2017-06-13 14:53:29 -0500

    test

--- util.js
+++ util.js
@@ -9,4 +9,60 @@ function JsonItem(jsonString,key)
 function GetRecentBuildId(jsonString,key)
 { 
   return JSON.parse(jsonString).value[0].id;
+} 
+function GetBlocksCovered(jsonString,key)
+{	
+	var obj=JSON.parse(jsonString);
+	var result={};
+	var sum=0;
+	for(var i=0, len=obj.value[0].modules.length; i < len; i++)
+	{
+		var item=obj.value[0].modules[i];
+		if(item.statistics && item.statistics.blocksCovered)
+		{
+			sum +=parseInt(item.statistics.blocksCovered);
+		}
+	}
+	result.blocksCovered=sum; 
+	sum=0;
+	for(var i=0, len=obj.value[0].modules.length; i < len; i++)
+	{
+		var item=obj.value[0].modules[i];
+		if(item.statistics && item.statistics.blocksNotCovered)
+		{
+			sum +=parseInt(item.statistics.blocksNotCovered);
+		}
+	}
+	result.blocksNotCovered=sum; 
+	sum=0;
+	for(var i=0, len=obj.value[0].modules.length; i < len; i++)
+	{
+		var item=obj.value[0].modules[i];
+		if(item.statistics && item.statistics.linesCovered)
+		{
+			sum +=parseInt(item.statistics.linesCovered);
+		}
+	}
+	result.linesCovered=sum;  
+	sum=0;
+	for(var i=0, len=obj.value[0].modules.length; i < len; i++)
+	{
+		var item=obj.value[0].modules[i];
+		if(item.statistics && item.statistics.linesNotCovered)
+		{
+			sum +=parseInt(item.statistics.linesNotCovered);
+		}
+	}
+	result.linesNotCovered=sum;  
+	sum=0;
+	for(var i=0, len=obj.value[0].modules.length; i < len; i++)
+	{
+		var item=obj.value[0].modules[i];
+		if(item.statistics && item.statistics.linesPartiallyCovered)
+		{
+			sum +=parseInt(item.statistics.linesPartiallyCovered);
+		}
+	}
+	result.linesPartiallyCovered=sum; 
+	return JSON.stringify(result);
 }
